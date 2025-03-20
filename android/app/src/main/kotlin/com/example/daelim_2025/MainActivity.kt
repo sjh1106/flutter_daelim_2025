1838e7d736216df269a45643ced37a2b02de7bbc
sjh1106 <sjh021106@gmail.com>
2025-03-20 10:15:57 +0900

    First commit

---
+++ android/app/src/main/kotlin/com/example/daelim_2025/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.daelim_2025
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/example.dart
@@ -0,0 +1,9 @@
+void main() {
+  final list = [0,1,2,3,4];
+
+  for (var i = 0; i < list.length; i++) {
+    print('1번째: $i');
+  }
+
+}
+
